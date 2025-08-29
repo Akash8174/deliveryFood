dbd184d0197a025faff01d716a1e048fee803901
Akash Prajapati <akashprajapati1028@gmail.com>
2025-08-30 00:26:57 +0530

    Update backend URL for live deployment

--- Admin-panel/src/App.jsx
+++ Admin-panel/src/App.jsx
@@ -7,7 +7,7 @@ import Add from './Pages/Add/Add'
 import Order from './Pages/Order/Order'
 import { ToastContainer} from 'react-toastify';
 const App = () => {
-  const url = "http://localhost:3000";
+  const url = "https://deliveryfood-k6k1.onrender.com";
   
   return (
     <div>
